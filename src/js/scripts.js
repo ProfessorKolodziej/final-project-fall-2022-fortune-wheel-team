c43c40bbc649790ba2b5c0a95be4ff3ac653db52
jiayuezhu9 <jiayue@bu.edu>
2022-11-30 21:49:43 -0500

    rough version

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,10 @@
 // Add your scripts here
+const button = document.querySelector('#spin-wheel');
+
+function spinWheel() {
+	const wheel = document.querySelector('.wheel');
+	wheel.classList.add('animate');
+	wheel.style.animationDuration = '500ms';
+}
+
+button.addEventListener('click', spinWheel);
